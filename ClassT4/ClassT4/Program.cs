5ccbf928d9b1ffe4f0efcbc27b4b877029d72f6f
tofiq7770 <tofigtn@code.edu.az>
2023-09-15 22:08:32 +0400

    Adding class task

---
+++ ClassT4/ClassT4/Program.cs
@@ -0,0 +1,234 @@
+﻿//int studentAge = 25;
+
+//string name1 = "XXX";
+//string name2 = "YYY";
+
+//string result = name2 + name1;
+
+//Console.WriteLine(result);
+
+
+
+//bool isOnline = true;
+
+//if (isOnline)
+//{
+//    Console.WriteLine("Yes");
+//}
+
+//else
+//{
+//    Console.WriteLine("No");
+//}
+
+
+
+//int age = 29;
+
+//if (age > 30)
+//{
+//    Console.WriteLine("Qocalirsan");
+//}
+//else if (age == 29)
+//{
+//    Console.WriteLine("Yaxsisan");
+//}
+
+//else
+//{
+//    Console.WriteLine("Gencsen");
+//}
+
+
+
+//int number = 30;
+
+//string result = number > 30 ? "Qocasan" : "Yaxsisan";
+//Console.WriteLine(result);
+
+//bool res = number > 30 ? true : false;
+
+//Console.WriteLine(res);
+
+
+//if (number > 30)
+//{
+
+//    Console.WriteLine(true);
+//}
+//else
+//{
+//    Console.WriteLine(false);
+//}
+
+
+
+//int x = 10, y = 100;
+
+//string result = x > y ? "x is greater than y" :
+//     x < y ? "x is less than y" :
+//     x == y ? "x is equal to y" : "No result";
+
+//Console.WriteLine(result);
+
+
+//string name = null;
+
+//var result = name ?? "XXX";
+
+//Console.WriteLine(name);
+///
+
+
+
+
+//Console.WriteLine("Add your email:");
+
+//string mail = Console.ReadLine();
+
+//Console.WriteLine("Add your password");
+
+//string password = Console.ReadLine();
+
+//if (mail == "haciahmadov.com" && password == "12345")
+
+
+//{
+//    Console.WriteLine("login success");
+//}
+
+//else
+//{
+
+//    Console.WriteLine("mail or password is wrong");
+//}
+
+
+
+//int weekDay = 2;
+//switch (weekDay)
+//{
+
+
+
+//    case 1:
+//        Console.WriteLine("Monday");
+//        break;
+
+
+//    case 2:
+//        Console.WriteLine("Tuesday");
+//        break;
+
+
+//    case 3:
+//        Console.WriteLine("Wensday");
+
+
+//        break;
+//    default:
+
+
+//        Console.WriteLine("not found");
+//        break;
+
+
+//}
+
+
+//int[] nums = { 3, 2, 6, 8, 9 };
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+
+//    Console.WriteLine(nums[i]);
+
+//}
+
+
+
+//int[] nums = { 3, 2, 6, 2, 9, 2 };
+//int count = 0;
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] == 2)
+//    {
+
+//        count++;
+
+
+//    }
+
+
+
+//}
+
+//Console.WriteLine(count);
+
+
+
+//int[] nums = { 3, 2, 6, 2, 9, 2 };
+
+//int count = 0;
+
+//int sumElem = 0;
+
+//int sumIndex = 0;
+
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] == 2)
+//    {
+//        sumElem += nums[i];
+
+//        sumIndex += i;
+
+//    }
+//}
+
+
+//int result = sumElem - sumIndex;
+
+
+//Console.WriteLine(result);
+
+
+
+//foreach (var item in nums)
+//{
+//    if (item == 2)
+//    {
+//        Console.WriteLine(item);
+//    }
+//}
+
+
+//DateTime date = DateTime.Now.AddDays(5);
+
+//Console.WriteLine(date);
+
+
+//string date = DateTime.Now.ToString("MM-dd-yyyy HH:mm");
+
+//Console.WriteLine(date);
+
+
+//int[] nums = { 8, 6, 3 };
+
+//int result = 1;
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+
+//    if (nums[1] % 2 == 0)
+//    {
+
+//        result = nums[i];
+//    }
+
+//}
+
+//Console.WriteLine(result);
+
